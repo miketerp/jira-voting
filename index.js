62a00d67fd710c15279ee5305cb732d98c3e2a65
Michael Longauer <michael.longauer@groupbyinc.com>
2017-03-30 15:57:34 -0400

    f

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html'); // <---- update to be pete's login screen
 });
 
+app.get('/admin', function(req, res){
+  res.sendFile(__dirname + '/test/admin.html');
+});
+
 app.get('/basic-test', function(req, res){
   res.sendFile(__dirname + '/test/index.html');
 });
@@ -48,26 +52,42 @@ io.on('connection', function(socket){
 });
 
 // SEARCHANDISER namespace
+let srUsers = {};
 sr.on('connection', function(socket){
+  srUsers[socket.id] = {};
   console.log('User connected to Searchandiser');
+  // console.log(sr.connected);
 
   socket.on('disconnect', function() {
     console.log('User disconnected from Searchandiser');
+    delete srUsers[this.id];
   });
 
   socket.on('vote', function(data) {
+    srUsers[this.id] = { name: data.name, vote: data.vote };
     const msg = data.name + ' on Searchandiser voted ' + data.vote;
     console.log(msg);
-    sr.emit('response', msg);
+
+    // time to tally the votes?
+    let voteTotal = votesAreIn(srUsers);
+    console.log(voteTotal);
+    if (voteTotal) {
+      for(let key in srUsers) {
+        sr.to(key).emit('response', 'All the votes are in! You voted ' + srUsers[key].vote + ', result was ' + voteTotal + '.');
+      }
+    }
   });
 });
 
 // STOREFRONT namespace
+let bbdUserCount = 0;
 bbd.on('connection', function(socket){
   console.log('User connected to Storefront');
+  bbdUserCount++;
 
   socket.on('disconnect', function() {
     console.log('User disconnected from Storefront');
+    bbdUserCount--;
   });
 
   socket.on('vote', function(data) {
@@ -78,11 +98,14 @@ bbd.on('connection', function(socket){
 });
 
 // WISDOM namespace
+let wsUserCount = 0;
 wis.on('connection', function(socket){
   console.log('User connected to Wisdom');
+  wsUserCount++;
 
   socket.on('disconnect', function() {
     console.log('User disconnected from Wisdom');
+    wsUserCount--;
   });
 
   socket.on('vote', function(data) {
@@ -93,11 +116,14 @@ wis.on('connection', function(socket){
 });
 
 // SRE namespace
+let sreUserCount = 0;
 sre.on('connection', function(socket){
   console.log('User connected to SRE');
+  sreUserCount++;
 
   socket.on('disconnect', function() {
     console.log('User disconnected from SRE');
+    sreUserCount--;
   });
 
   socket.on('vote', function(data) {
@@ -106,4 +132,22 @@ sre.on('connection', function(socket){
     sre.emit('response', msg);
   });
 });
+
+/**
+ *  FUNCTIONS
+ */
+
+const votesAreIn = (users) => {
+      console.log(users);
+  let total = 0;
+  for (let key in users) {
+    if (users[key].vote != undefined) {
+      total += parseInt(users[key].vote);
+    } else {
+      return false;
+    }
+  }
+  console.log('Total: ' + total);
+    return Math.round(total / Object.keys(users).length);
+};
 
