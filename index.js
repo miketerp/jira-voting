f436646d6e9588c6ac8cb90f1bd7ad3a9b244f96
Michael Longauer <michael.longauer@groupbyinc.com>
2017-03-30 18:44:28 -0400

    each namespace can have an admin

--- index.js
+++ index.js
@@ -1,78 +1,86 @@
-const app = require('express')();
+const app  = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
-const sr = io.of('/searchandiser');
-const bbd = io.of('/storefront');
-const wis = io.of('/wisdom');
-const sre = io.of('/site-reliability');
+const io   = require('socket.io')(http);
+const sr   = io.of('/searchandiser');
+const bbd  = io.of('/storefront');
+const wis  = io.of('/wisdom');
+const sre  = io.of('/site-reliability');
 
-http.listen(20380, function(){
+http.listen(20380, function () {
   console.log('Started listening on port 20380...');
 });
-
 
 /**
  * EXPRESS ROUTING
  */
 
 // Serve the login client page. They should be redirected depending on their role
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html'); // <---- update to be pete's login screen
 });
 
-app.get('/admin', function(req, res){
+app.get('/admin', function (req, res) {
   res.sendFile(__dirname + '/test/admin.html');
 });
 
-app.get('/basic-test', function(req, res){
+app.get('/basic-test', function (req, res) {
   res.sendFile(__dirname + '/test/index.html');
 });
 
-app.get('/name-test', function(req, res){
+app.get('/name-test', function (req, res) {
   res.sendFile(__dirname + '/test/name-test.html');
 });
-
 
 /**
  * SOCKET.IO NAMESPACES
  */
 
 // DEFAULT namespace
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
   console.log('connection established');
 
-  socket.on('vote', function(data) {
+  socket.on('vote', function (data) {
     console.log(data);
     io.emit('response', data);
   });
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     console.log('user disconnected');
   });
 });
 
 // SEARCHANDISER namespace
 let srUsers = {};
-sr.on('connection', function(socket){
+sr.on('connection', function (socket) {
   srUsers[socket.id] = {};
   console.log('User connected to Searchandiser');
-  // console.log(sr.connected);
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     console.log('User disconnected from Searchandiser');
     delete srUsers[this.id];
   });
 
-  socket.on('vote', function(data) {
-    srUsers[this.id] = { name: data.name, vote: data.vote };
-    const msg = data.name + ' on Searchandiser voted ' + data.vote;
+  socket.on('adminIsIn', function () {
+    console.log('Administrator has logged into SR');
+    srUsers[this.id] = { admin: true };
+  });
+
+  socket.on('vote', function (data) {
+    srUsers[this.id] = {name: data.name, vote: data.vote, admin: false};
+    const msg        = data.name + ' on Searchandiser voted ' + data.vote;
     console.log(msg);
+
+    let admin = hasAdmin(bbdUsers)
+    console.log('Admin: ' + admin);
+    if (admin) {
+      bbd.to(admin).emit('voter voted', data.name + ': ' + data.vote);
+    }
 
     // time to tally the votes?
     let voteTotal = votesAreIn(srUsers);
     console.log(voteTotal);
     if (voteTotal) {
-      for(let key in srUsers) {
+      for (let key in srUsers) {
         sr.to(key).emit('response', 'All the votes are in! You voted ' + srUsers[key].vote + ', result was ' + voteTotal + '.');
       }
     }
@@ -80,56 +88,116 @@ sr.on('connection', function(socket){
 });
 
 // STOREFRONT namespace
-let bbdUserCount = 0;
-bbd.on('connection', function(socket){
+let bbdUsers = {};
+bbd.on('connection', function (socket) {
+  bbdUsers[socket.id] = {};
   console.log('User connected to Storefront');
-  bbdUserCount++;
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     console.log('User disconnected from Storefront');
-    bbdUserCount--;
+    delete bbdUsers[this.id];
   });
 
-  socket.on('vote', function(data) {
-    const msg = data.name + ' on Storefront voted ' + data.vote;
+  socket.on('adminIsIn', function () {
+    console.log('Administrator has logged into BBD');
+    bbdUsers[this.id] = { admin: true };
+  });
+
+  socket.on('vote', function (data) {
+    bbdUsers[this.id] = {name: data.name, vote: data.vote, admin: false};
+    const msg         = data.name + ' on Storefront voted ' + data.vote;
     console.log(msg);
-    bbd.emit('response', msg);
+
+    let admin = hasAdmin(bbdUsers)
+    console.log('Admin: ' + admin);
+    if (admin) {
+      bbd.to(admin).emit('voter voted', data.name + ': ' + data.vote);
+    }
+
+    // time to tally the votes?
+    let voteTotal = votesAreIn(bbdUsers);
+    console.log(voteTotal);
+    if (voteTotal) {
+      for (let key in bbdUsers) {
+        bbd.to(key).emit('response', 'All the votes are in! You voted ' + bbdUsers[key].vote + ', result was ' + voteTotal + '.');
+      }
+    }
   });
 });
 
 // WISDOM namespace
-let wsUserCount = 0;
-wis.on('connection', function(socket){
+let wisUsers = {};
+wis.on('connection', function (socket) {
+  wisUsers[socket.id] = {};
   console.log('User connected to Wisdom');
-  wsUserCount++;
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     console.log('User disconnected from Wisdom');
-    wsUserCount--;
+    delete wisUsers[this.id];
   });
 
-  socket.on('vote', function(data) {
-    const msg = data.name + ' on Wisdom voted ' + data.vote;
+  socket.on('adminIsIn', function () {
+    console.log('Administrator has logged into WIS');
+    srUsers[this.id] = { admin: true };
+  });
+
+  socket.on('vote', function (data) {
+    wisUsers[this.id] = {name: data.name, vote: data.vote, admin: false};
+    const msg         = data.name + ' on Wisdom voted ' + data.vote;
     console.log(msg);
-    wis.emit('response', msg);
+
+    let admin = hasAdmin(bbdUsers)
+    console.log('Admin: ' + admin);
+    if (admin) {
+      bbd.to(admin).emit('voter voted', data.name + ': ' + data.vote);
+    }
+
+    // time to tally the votes?
+    let voteTotal = votesAreIn(wisUsers);
+    console.log(voteTotal);
+    if (voteTotal) {
+      for (let key in wisUsers) {
+        wis.to(key).emit('response', 'All the votes are in! You voted ' + wisUsers[key].vote + ', result was ' + voteTotal + '.');
+      }
+    }
   });
 });
 
 // SRE namespace
-let sreUserCount = 0;
-sre.on('connection', function(socket){
+let sreUsers = {};
+sre.on('connection', function (socket) {
+  sreUsers[socket.id] = {};
   console.log('User connected to SRE');
-  sreUserCount++;
 
-  socket.on('disconnect', function() {
+  socket.on('disconnect', function () {
     console.log('User disconnected from SRE');
-    sreUserCount--;
+    delete sreUsers[this.id];
   });
 
-  socket.on('vote', function(data) {
+  socket.on('adminIsIn', function () {
+    console.log('Administrator has logged into SRE');
+    srUsers[this.id] = { admin: true };
+  });
+
+  socket.on('vote', function (data) {
+    sreUsers[this.id] = {name: data.name, vote: data.vote, admin: false};
     const msg = data.name + ' on SRE voted ' + data.vote;
     console.log(msg);
-    sre.emit('response', msg);
+
+    let admin = hasAdmin(bbdUsers)
+    console.log('Admin: ' + admin);
+    if (admin) {
+      bbd.to(admin).emit('voter voted', data.name + ': ' + data.vote);
+    }
+
+    // time to tally the votes?
+    let voteTotal = votesAreIn(sreUsers);
+    console.log(voteTotal);
+    if (voteTotal) {
+      for (let key in sreUsers) {
+        sre.to(key).emit('response', 'All the votes are in! You voted ' + sreUsers[key].vote + ', result was ' + voteTotal + '.');
+      }
+    }
   });
 });
 
@@ -138,16 +206,25 @@ sre.on('connection', function(socket){
  */
 
 const votesAreIn = (users) => {
-      console.log(users);
+  console.log(users);
   let total = 0;
   for (let key in users) {
     if (users[key].vote != undefined) {
       total += parseInt(users[key].vote);
-    } else {
+    } else if (!users[key].admin) {
       return false;
     }
   }
   console.log('Total: ' + total);
-    return Math.round(total / Object.keys(users).length);
+  return Math.round(total / Object.keys(users).length);
+};
+
+const hasAdmin = (users) => {
+  for (let key in users) {
+    if (users[key].admin) {
+      return key;
+    }
+  }
+  return false;
 };
 
